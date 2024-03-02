3a4e94cd8626f4ee559ee142540616ca82e42f3c
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-03-02 03:38:12 -0300

    adding a retry logic

--- app/database/openDB.go
+++ app/database/openDB.go
@@ -13,16 +13,24 @@ var DB *gorm.DB
 var err error
 
 func StartDB() {
-	time.Sleep(3)
-
 	dsn := utils.GenerateDSN()
 
 	log.Printf("Unsecure show me the DSN: %s", dsn)
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	for i := 1; i < 11; i++ {
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+		if err == nil {
+			break
+		}
+
+		log.Printf("Error: Unknown error opening gorm: %s", err.Error())
+		log.Printf("Retrying in %d seconds...", i)
+		time.Sleep(time.Duration(i) * time.Second)
+	}
 
 	if err != nil {
-		log.Fatalf("Error: Unknown error opening gorm: %s", err.Error())
+		log.Fatalf("Error: Failed to connect to database after multiple attempts: %s", err.Error())
 	}
 
 	sqlDB, err := DB.DB()
